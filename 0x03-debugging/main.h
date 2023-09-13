493299df97a9e749fbddb5c5aa0794d35263ffbf
Sheu Sadiq Olalekan <quadrisodiqolamilekan@gmail.com>
2023-09-13 01:56:36 -0700

    Debugging

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_N
+#define MAIN_N
+#include <stdio.h>
+
+void positve_or_negative(int i);
+int largest_number(int a, int b , int c);
+void printf_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+
+#endif
